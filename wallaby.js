86ef401a93e20d6ff0f75ddb5a662f1ae6c416e7
Raul Macarie <raul.macarie@gmail.com>
2017-07-11 06:01:19 +0300

    Fix wallaby not showing coverage

--- wallaby.js
+++ wallaby.js
@@ -1,18 +1,24 @@
-module.exports = function (wallaby) {
-  return {
-    files: [
-      'src/**/*.js',
-    ],
+module.exports = wallaby => ({
+  files: [
+    'src/**/*.js',
+    'jest.config.js',
+  ],
 
-    tests: ['test/**/*spec.js'],
-    compilers: {
-      '**/*.js*': wallaby.compilers.babel(),
-    },
-    env: {
-      type: 'node',
-      runner: 'node',
-    },
+  tests: ['test/**/*spec.js'],
+  compilers: {
+    '**/*.js*': wallaby.compilers.babel(),
+  },
+  env: {
+    type: 'node',
+    runner: 'node',
+  },
 
-    testFramework: 'jest',
-  };
-};
+  testFramework: 'jest',
+
+  setup: (w) => {
+    const jestConfig = require('./jest.config');
+    jestConfig.modulePaths[0] = w.projectCacheDir;
+    w.testFramework.configure(jestConfig);
+  },
+  debug: true,
+});
